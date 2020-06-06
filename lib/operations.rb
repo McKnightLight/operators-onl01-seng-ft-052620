057e71f4893d3a600c4dae03af94d393560cc881
Jonathan McKnight <themcknightrider@gmail.com>
2020-06-06 15:14:11 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,9 +2,13 @@ require 'pry'
 
 def unsafe?(speed)
   #binding.pry 
-  speed > 60 || speed < 40
+  if speed > 60 || speed < 40
+    true 
+  else 
+    false 
+  end 
 end
 
 def not_safe?(speed)
-	
+	speed > 60 || speed < 40 ? true : false 
 end
